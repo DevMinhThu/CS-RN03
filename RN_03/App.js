d44af659dd21faa1d93b7ff54da83d12f53ff712
Vũ Minh Thư <devminhthu2023@gmail.com>
2021-12-15 21:32:48 +0700

    fix/update-package.json

--- RN_03/App.js
+++ RN_03/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 // import ExerciseStyleSheet from './src/Lec01/ExerciseStyleSheet';
 // import DemoStyleSheet from './src/Lec02/DemoStyleSheet';
 // import DemoFlex from './src/Lec02/DemoFlex';
@@ -16,10 +17,17 @@ import React from 'react';
 // import DemoCallAPI from './src/Buoi8/DemoCallAPI/index';
 // import DemoLifecycle from './src/Buoi8/DemoLifecycle/index';
 // import DemoAxios from './src/Buoi9/DemoAxios';
+import rootStore from './src/Buoi7/redux/store';
+import RockPaperScissorRedux from './src/Buoi7/RockPaperScissorRedux';
 import ListProductScreen from './src/Buoi9/ListProductScreen';
 
 const App = () => {
-  return <ListProductScreen />;
+  return (
+    <Provider store={rootStore}>
+      <RockPaperScissorRedux />
+      {/* <ListProductScreen /> */}
+    </Provider>
+  );
 };
 
 export default App;
